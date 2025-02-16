ee01348248415313c58102d643aad419112a227d
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-02-15 23:55:28 -0500

    File updated at 2/15/2025, 11:55:28 PM

--- 1718-ConstructtheLexicographicallyLargestValidSequence/1718-ConstructtheLexicographicallyLargestValidSequence.java
+++ 1718-ConstructtheLexicographicallyLargestValidSequence/1718-ConstructtheLexicographicallyLargestValidSequence.java
@@ -8,29 +8,25 @@ class Solution {
 
     private boolean backtrack(int[] result, boolean[] used, int index, int n) {
         // If all positions are filled, return true
-        if (index == result.length) {
+        if (index == result.length) 
             return true;
-        }
 
         // If the current position is already filled, move to the next position
-        if (result[index] != 0) {
+        if (result[index] != 0) 
             return backtrack(result, used, index + 1, n);
-        }
 
         // Try placing numbers from n to 1
         for (int num = n; num >= 1; num--) {
             // Skip if the number is already used
-            if (used[num]) {
+            if (used[num]) 
                 continue;
-            }
 
             // Handle number 1 separately (it only appears once)
             if (num == 1) {
                 result[index] = 1;
                 used[1] = true;
-                if (backtrack(result, used, index + 1, n)) {
+                if (backtrack(result, used, index + 1, n)) 
                     return true;
-                }
                 used[1] = false;
                 result[index] = 0;
             } else {
@@ -39,9 +35,8 @@ class Solution {
                     result[index] = num;
                     result[index + num] = num;
                     used[num] = true;
-                    if (backtrack(result, used, index + 1, n)) {
+                    if (backtrack(result, used, index + 1, n)) 
                         return true;
-                    }
                     used[num] = false;
                     result[index] = 0;
                     result[index + num] = 0;
