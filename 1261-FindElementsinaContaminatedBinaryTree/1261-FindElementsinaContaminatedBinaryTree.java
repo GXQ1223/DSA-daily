711c1079a728af62fb135311d00e9f585f2209cb
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-02-20 22:35:49 -0500

    File updated at 2/20/2025, 10:35:48 PM

--- 1261-FindElementsinaContaminatedBinaryTree/1261-FindElementsinaContaminatedBinaryTree.java
+++ 1261-FindElementsinaContaminatedBinaryTree/1261-FindElementsinaContaminatedBinaryTree.java
@@ -15,32 +15,30 @@
  */
 class FindElements {
     TreeNode r;
+    Set<Integer> set;
 
     public FindElements(TreeNode root) {
         this.r = root;
         root.val = 0;
+        this.set = new HashSet<>();
+        this.set.add(0);
         dfs(root);
     }
     
     public boolean find(int target) {
-        return search(this.r, target);
-    }
-
-    public boolean search(TreeNode root, int target){
-        if(root == null) return false;
-        if(root.val == target) return true;
-        return search(root.left, target) || search(root.right, target);
-
+        return this.set.contains(target);
     }
 
     public void dfs(TreeNode root){
         if(root.left != null && root.left.val < 0){
             root.left.val = root.val * 2 + 1;
+            this.set.add(root.left.val);
             // System.out.println("root left: " + root.left.val);
             dfs(root.left);
         }
         if(root.right != null && root.right.val < 0){
             root.right.val = root.val * 2 + 2;
+            this.set.add(root.right.val);
             // System.out.println("root right: " + root.right.val);
             dfs(root.right);
         }
