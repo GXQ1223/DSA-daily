6db7451e6ffaa66eba09daafa1857f880256304c
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-03-02 13:22:21 -0500

    File updated at 3/2/2025, 1:22:21 PM

--- 1092-ShortestCommonSupersequence/1092-ShortestCommonSupersequence.java
+++ 1092-ShortestCommonSupersequence/1092-ShortestCommonSupersequence.java
@@ -1,45 +1,31 @@
 class Solution {
     public String shortestCommonSupersequence(String str1, String str2) {
-        int[][] dp = new int[str1.length() + 1][str2.length() + 1];
-        
-        // Initialize dp table
-        for (int i = 1; i <= str1.length(); i++) {
-            for (int j = 1; j <= str2.length(); j++) {
-                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
+        int m = str1.length();
+        int n = str2.length();
+        int[][]dp = new int[m+1][n+1];
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(str1.charAt(i-1) == str2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + 1;
                 } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
-
-        int i = str1.length();
-        int j = str2.length();
         StringBuilder sb = new StringBuilder();
-                while (i > 0 && j > 0) {
-            if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
-                sb.append(str1.charAt(i - 1));
-                i--;
-                j--;
-            } else if (dp[i - 1][j] > dp[i][j - 1]) {
-                sb.append(str1.charAt(i - 1));
-                i--;
+        int i = m, j = n;
+        while(i > 0 && j > 0){
+            if(str1.charAt(i-1) == str2.charAt(j-1)){
+                sb.insert(0, str1.charAt(i-1)); i--; j--;
+            } else if(dp[i-1][j] > dp[i][j-1]){
+                sb.insert(0, str1.charAt(i-1)); i--;
             } else {
-                sb.append(str2.charAt(j - 1));
-                j--;
+                sb.insert(0, str2.charAt(j-1)); j--;
             }
         }
 
-        // Append the remaining characters from str1 or str2
-        while (i > 0) {
-            sb.append(str1.charAt(i - 1));
-            i--;
-        }
-        while (j > 0) {
-            sb.append(str2.charAt(j - 1));
-            j--;
-        }
-
-        return sb.reverse().toString();
+        while(i > 0){sb.insert(0, str1.charAt(i-1)); i--;}
+        while(j > 0){sb.insert(0, str2.charAt(j-1)); j--;}
+        return sb.toString();
     }
 }
