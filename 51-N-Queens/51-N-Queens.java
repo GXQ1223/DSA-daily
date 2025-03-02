85c6af0bdf9e5eb960e8ce67b0cea429ceee3336
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-03-02 13:46:19 -0500

    File updated at 3/2/2025, 1:46:19 PM

--- 51-N-Queens/51-N-Queens.java
+++ 51-N-Queens/51-N-Queens.java
@@ -1,54 +1,41 @@
 class Solution {
-    Set<List<String>> res;
+    List<List<String>> res;
+
     public List<List<String>> solveNQueens(int n) {
-        res = new HashSet<>();
+        res = new LinkedList<>();
+
         int[][]matrix = new int[n][n];
-
         boolean[]cols = new boolean[n];
-        boolean[]ups = new boolean[n * 2 - 1];
-        boolean[]dns = new boolean[n * 2 - 1];
+        boolean[]rows = new boolean[n];
+        boolean[]ups = new boolean[n * 2 + 1];
+        boolean[]dns = new boolean[n * 2 + 1];
 
-        backtrack(0, matrix, cols, ups, dns, n);
-        
-        List<List<String>> resList = new ArrayList<>();
-        for(List<String>r: res) resList.add(r);
-        return resList;
+        backtrack(0, rows, cols, ups, dns, matrix, n);
+        return res;
     }
 
-    public void backtrack(
-        int row, 
-        int[][]matrix, 
-        boolean[]cols, 
-        boolean[]ups, boolean[]dns, int n){
-        if(row == n){
-            res.add(makingList(matrix));
-            return;
+    public void backtrack(int currRow, boolean[]rows, boolean[]cols, boolean[]ups, boolean[]dns, int[][]matrix, int n){
+        if(currRow == n){
+            res.add(toStringList(matrix));
         }
-
-        for(int c = 0; c < n; c++){
-            if(!cols[c] && !ups[row + c] && !dns[c - row + n - 1]){
-                matrix[row][c] = 1;
-                cols[c] = true;
-                ups[row + c] = true;
-                dns[c - row + n - 1] = true;
-
-                backtrack(row+1,matrix, cols, ups, dns, n);
-
-                matrix[row][c] = 0;
-                cols[c] = false;
-                ups[row + c] = false;
-                dns[c - row + n - 1] = false;
+        for(int i = 0; i < n; i++){
+            if(!cols[i] && !ups[currRow + i] && !dns[n - 1 + i - currRow]){ // 4 - 1 + 0 - 3 = 0
+                cols[i] = true; ups[currRow + i] = true; dns[n - 1 + i - currRow] = true;
+                matrix[currRow][i] = 1;
+                backtrack(currRow + 1, rows, cols, ups, dns, matrix, n);
+                matrix[currRow][i] = 0;
+                cols[i] = false; ups[currRow + i] = false; dns[n - 1 + i - currRow] = false;
             }
         }
     }
 
-    public List<String> makingList(int[][]matrix){
-        int n = matrix.length;
-        List<String> res = new ArrayList<>();
-        for(int i = 0; i < n; i++){
+    public List<String> toStringList(int[][]matrix){
+        List<String>res = new ArrayList<>();
+        for(int i = 0; i < matrix.length;i++){
             StringBuilder sb = new StringBuilder();
-            for(int j = 0; j < n; j++)
-                sb.append(matrix[i][j] == 1 ? 'Q' : '.');
+            for(int j = 0; j < matrix.length;j++){
+                sb.append(matrix[i][j] == 0 ? '.' : 'Q');
+            }
             res.add(sb.toString());
         }
         return res;
