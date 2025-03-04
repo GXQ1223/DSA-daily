1175f764cca1d955024f4242c122a940d871496c
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-03-03 19:57:32 -0500

    File updated at 3/3/2025, 7:57:32 PM

--- 1780-CheckifNumberisaSumofPowersofThree/1780-CheckifNumberisaSumofPowersofThree.java
+++ 1780-CheckifNumberisaSumofPowersofThree/1780-CheckifNumberisaSumofPowersofThree.java
@@ -2,16 +2,13 @@ class Solution {
     public boolean checkPowersOfThree(int n) {
         int[]nums = new int[17];
         nums[0] = 1;
-        for(int i = 1; i < 17; i++){
+        for(int i = 1; i < 17; i++)
             nums[i] = 3 * nums[i-1]; 
-            System.out.print(";" + nums[i]);
-        }
-        for(int i = 16; i >= 0; i--){
-            if(nums[i] <= n){
+        
+        for(int i = 16; i >= 0; i--)
+            if(nums[i] <= n)
                 n -= nums[i];
-            }
-            System.out.println("n: " + n);
-        }
+
         return n == 0;
     }
 }
