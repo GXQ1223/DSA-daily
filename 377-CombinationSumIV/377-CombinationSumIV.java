9596913cd904b37048321ae73a96de5f5e67780e
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-03-11 15:50:13 -0400

    File updated at 3/11/2025, 3:50:13 PM

--- 377-CombinationSumIV/377-CombinationSumIV.java
+++ 377-CombinationSumIV/377-CombinationSumIV.java
@@ -2,7 +2,7 @@ class Solution {
     public int combinationSum4(int[] nums, int target) {
         Arrays.sort(nums);
         int[]dp = new int[target + 1];
-        dp[0] = 1;;
+        dp[0] = 1;
 
         for(int combSum = 1; combSum < target+1; combSum++){
             for(int num: nums){
