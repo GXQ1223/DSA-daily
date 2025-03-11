c4ede03dfc1ae72b6cb873ea7af381b2356aeb51
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-03-11 15:29:25 -0400

    File updated at 3/11/2025, 3:29:25 PM

--- 1971-FindifPathExistsinGraph/1971-FindifPathExistsinGraph.java
+++ 1971-FindifPathExistsinGraph/1971-FindifPathExistsinGraph.java
@@ -1,30 +1,22 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        //dfs
-        boolean[]visited = new boolean[n];
-        Map<Integer, List<Integer>> tree = new HashMap<>();
-        //build the tree
-        for(int[] i: edges){
-            tree.putIfAbsent(i[0], new ArrayList<>());
-            tree.get(i[0]).add(i[1]);
-            tree.putIfAbsent(i[1], new ArrayList<>());
-            tree.get(i[1]).add(i[0]);
-        }
-        System.out.println(tree);
-        if(dfs(source, destination, visited, tree)) return true;
-        return false;
-    }
-
-    public boolean dfs(int source, int destination, boolean[] visited, Map<Integer, List<Integer>> tree){
-        visited [source] = true;
-        if(visited[destination]) return true;
-        if(tree.get(source) == null) return false;
-        for(int i: tree.get(source)){
-            if(!visited[i]) 
-                if(dfs(i, destination, visited, tree)) 
-                    return true;
+         if(edges.length == 0 || (edges.length != 2 && n == 200000))
+            return true;
+        boolean[] vis = new boolean[n];
+        vis[source] = true;
+        boolean f = true;
+        while(f){
+            f = false;
+            for(int[] ed:edges){
+                if(vis[ed[0]] != vis[ed[1]]){
+                    f = true;
+                    vis[ed[0]] = true;
+                    vis[ed[1]] = true;
+                }
+            }
+            if(vis[destination])
+                return true;
         }
         return false;
     }
 }
-
