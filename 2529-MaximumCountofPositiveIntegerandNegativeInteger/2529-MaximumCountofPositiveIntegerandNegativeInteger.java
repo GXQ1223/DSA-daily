9fc018f307e70a9efacfa242bd4cd83f6e5b833b
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-03-11 23:56:25 -0400

    File updated at 3/11/2025, 11:56:25 PM

--- 2529-MaximumCountofPositiveIntegerandNegativeInteger/2529-MaximumCountofPositiveIntegerandNegativeInteger.java
+++ 2529-MaximumCountofPositiveIntegerandNegativeInteger/2529-MaximumCountofPositiveIntegerandNegativeInteger.java
@@ -1,22 +1,43 @@
 class Solution {
-    public int maximumCount(int[] nums) {
-        int n = nums.length;
-        int first = 0;
-        int second = 0;
-        for(int i = 0; i < n; i++){
-            if(nums[i] < 0){
-                first = i+1;
-            } else break;
-        }
-        System.out.println(first);
 
-        for(int i = first; i < n; i++){
-            if(nums[i] > 0){
-                second = n - i + 0;
-                break;
+    public static int search(int[] nums,int start,int end){
+        int countNeg = 0,countPos = 0,countZero = 0;
+         while(start<=end){
+            int mid = end+(start-end)/2;
+            if(nums[mid] == 0){
+                countZero++;
+                if(countZero == nums.length) return 0;
+                if(mid > 0 && nums[mid-1] < 0){
+                    countNeg = mid;
+                    start = mid+1;
+                }else if(mid < nums.length-1 && nums[mid+1] > 0){
+                    countPos = nums.length-mid-1;
+                    end = mid-1;
+                }
+                else if(nums[0] == 0 && nums[nums.length-1] != 0){
+                    start = mid+1;
+                }
+                else if(nums[0] != 0 && nums[nums.length-1] == 0){
+                    end = mid-1;
+                }
+                
+            }
+            else if(nums[mid] < 0){
+                countNeg = mid+1;
+                start = mid+1;
+            }else if(nums[mid] > 0){
+                countPos = nums.length-mid;
+                end = mid-1;
             }
         }
-        return Math.max(first, second);
 
+        if(countNeg > countPos) return countNeg;  
+        return countPos;
+    }
+
+    public int maximumCount(int[] nums) {
+       int start = 0,end = nums.length-1;
+       start = search(nums,start,end);
+       return start;
     }
 }
