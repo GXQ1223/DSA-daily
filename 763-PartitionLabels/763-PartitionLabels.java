3bec5d4e060c31bd80d71e735d898931c5c682f3
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-03-29 20:38:36 -0400

    File updated at 3/29/2025, 8:38:36 PM

--- 763-PartitionLabels/763-PartitionLabels.java
+++ 763-PartitionLabels/763-PartitionLabels.java
@@ -1,4 +1,4 @@
-// Last updated: 3/29/2025, 8:38:20 PM
+// Last updated: 3/29/2025, 8:38:36 PM
 class Solution {
     public List<Integer> partitionLabels(String s) {
         int[][]grid = new int[s.length()][26];
@@ -20,19 +20,14 @@ class Solution {
         // for(int i = 0; i )
         List<Integer> res = new LinkedList<>();
         int end = pq.peek()[1];
-        // System.out.println("start: " + pq.peek()[0]);
-        // System.out.println("end: " + end);
         int currentLen = 0;
         while(!pq.isEmpty()){
-            if(!pq.isEmpty())
-                System.out.println("pq: " + pq.peek()[0] + "; " + pq.peek()[1]);
             if(pq.peek()[0] > end) {
                 int top = pq.peek()[0]; 
                 if(res.size() != 0) 
                     top -= currentLen;; 
                 res.add(top);
                 currentLen += top;
-                System.out.println("currentLen: " + currentLen);
             }
             end = Math.max(end, pq.poll()[1]); 
         }
