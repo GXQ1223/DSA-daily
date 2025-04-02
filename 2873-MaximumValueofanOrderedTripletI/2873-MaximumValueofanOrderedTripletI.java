4ffcc3be3f16df6940de7c8ce71bdf02160dd4ae
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-04-01 20:05:00 -0400

    File updated at 4/1/2025, 8:05:00 PM

--- 2873-MaximumValueofanOrderedTripletI/2873-MaximumValueofanOrderedTripletI.java
+++ 2873-MaximumValueofanOrderedTripletI/2873-MaximumValueofanOrderedTripletI.java
@@ -1,15 +1,12 @@
-// Last updated: 4/1/2025, 8:04:32 PM
+// Last updated: 4/1/2025, 8:05:00 PM
 class Solution {
     public long maximumTripletValue(int[] nums) {
         long res = 0;
         int n = nums.length;
-        for(int i = 0; i < n; i++){
-            for(int j = i+1; j < n; j++){
-                for(int k = j+1; k < n; k++){
-                    res = Math.max(res, ((long)nums[i] - (long)nums[j]) * (long)nums[k]);
-                }
-            }
-        }
+        for(int i = 0; i < n; i++)
+            for(int j = i+1; j < n; j++)
+                for(int k = j+1; k < n; k++)
+                    res = Math.max(res, (long)(nums[i] - nums[j]) * nums[k]);
         return res;
     }
 }
