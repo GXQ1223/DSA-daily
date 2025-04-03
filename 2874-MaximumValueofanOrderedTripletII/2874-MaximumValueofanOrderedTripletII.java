f9f591a1d5006aa42a65009a10a6118fcad85d11
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-04-03 00:05:22 -0400

    File updated at 4/3/2025, 12:05:22 AM

--- 2874-MaximumValueofanOrderedTripletII/2874-MaximumValueofanOrderedTripletII.java
+++ 2874-MaximumValueofanOrderedTripletII/2874-MaximumValueofanOrderedTripletII.java
@@ -1,22 +1,17 @@
-// Last updated: 4/3/2025, 12:04:31 AM
+// Last updated: 4/3/2025, 12:05:22 AM
 class Solution {
     public long maximumTripletValue(int[] nums) {
         int n = nums.length;
-        long[]pref = new long[n];
-        long[]suff = new long[n];
+        int[]pref = new int[n];
+        int[]suff = new int[n];
 
-        pref[1] = (long)nums[0];
+        pref[1] = nums[0];
         for(int i = 2; i < n; i++)
-            pref[i] = Math.max(pref[i-1], (long)nums[i-1]);
+            pref[i] = Math.max(pref[i-1], nums[i-1]);
 
-// for(int i = 0; i < n; i++)System.out.print(pref[i] + "; ");
-
-        suff[n-2] = (long)nums[n-1];
+        suff[n-2] = nums[n-1];
         for(int i = n - 3; i >= 0; i--)
-            suff[i] = Math.max(suff[i+1], (long)nums[i+1]);
-// System.out.println();
-// System.out.println("suff: ");
-// for(int i = 0; i < n; i++)System.out.print(suff[i] + "; ");
+            suff[i] = Math.max(suff[i+1], nums[i+1]);
 
         long res = 0;
         for(int i = 1; i <= n-2; i++)
