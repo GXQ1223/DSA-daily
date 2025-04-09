8b78db58de1cf7a2a92e073dfb17adacc7e5dc02
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-04-08 21:20:12 -0400

    File updated at 4/8/2025, 9:20:12 PM

--- 3375-MinimumOperationstoMakeArrayValuesEqualtoK/3375-MinimumOperationstoMakeArrayValuesEqualtoK.java
+++ 3375-MinimumOperationstoMakeArrayValuesEqualtoK/3375-MinimumOperationstoMakeArrayValuesEqualtoK.java
@@ -1,8 +1,15 @@
-// Last updated: 4/8/2025, 9:19:36 PM
+// Last updated: 4/8/2025, 9:20:12 PM
 class Solution {
     public int minOperations(int[] nums, int k) {
-        Set<Integer>set = new HashSet<>(); boolean yes = false;
-        for(int i: nums){set.add(i); if(i < k) return -1; if(i == k) yes = true;}
+        Set<Integer>set = new HashSet<>(); 
+        boolean yes = false;
+        for(int i: nums){
+            if(i < k) 
+                return -1; 
+            if(i == k) 
+                yes = true;
+            set.add(i); 
+        }
         return yes? set.size() - 1 : set.size() ;
     }
 }
