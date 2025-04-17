8ce1e7f93199ac071b307ccf75481a27a218823d
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-04-16 22:20:11 -0400

    File updated at 4/16/2025, 10:20:11 PM

--- 2176-CountEqualandDivisiblePairsinanArray/2176-CountEqualandDivisiblePairsinanArray.java
+++ 2176-CountEqualandDivisiblePairsinanArray/2176-CountEqualandDivisiblePairsinanArray.java
@@ -1,14 +1,10 @@
-// Last updated: 4/16/2025, 10:19:58 PM
+// Last updated: 4/16/2025, 10:20:11 PM
 class Solution {
     public int countPairs(int[] nums, int k) {
         int n = nums.length, res = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = i + 1; j < n; j++){
-                if(nums[i] == nums[j] && (i * j) % k == 0){
-                    res++;
-                }
-            }
-        }
+        for(int i = 0; i < n; i++)
+            for(int j = i + 1; j < n; j++)
+                if(nums[i] == nums[j] && (i * j) % k == 0) res++;
         return res;
     }
 }
