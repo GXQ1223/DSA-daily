367c9c9c901f32ddae1a6041fe101d044bb1fdf6
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-04-19 21:52:00 -0400

    Create file

---
+++ 781-RabbitsinForest/781-RabbitsinForest.java
@@ -0,0 +1,23 @@
+// Last updated: 4/19/2025, 9:52:00 PM
+class Solution {
+    public int numRabbits(int[] answers) {
+        //if answer[i] == 1, 1 needs another 1 to match with it. so total 2 rabbits, each has 1 same color rabbit
+        //if answer[i] == 2, 2 needs another two 2's to match with it. so total 3 rabbits, each has 2 same color rabbit
+        //
+        // Arrays.sort(answers);
+        int[]freq = new int[1000];
+        for(int i = 0; i < answers.length; i++){
+            freq[answers[i]]++;
+        }
+        for(int i: freq) System.out.println(i);
+        int res = 0;
+        for(int i = 0; i < freq.length; i++){
+            if(freq[i] == 0) continue;
+            while(freq[i] > 0){
+                res += (i+1);
+                freq[i] -= (i+1);
+            }
+        }
+        return res;
+    }
+}
