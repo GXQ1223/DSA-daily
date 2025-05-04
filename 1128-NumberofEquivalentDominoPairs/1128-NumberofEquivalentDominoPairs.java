8c4c9e535f59d52abbec4f6482db37b66e99c758
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-05-04 12:23:01 -0400

    File updated at 5/4/2025, 12:23:01 PM

--- 1128-NumberofEquivalentDominoPairs/1128-NumberofEquivalentDominoPairs.java
+++ 1128-NumberofEquivalentDominoPairs/1128-NumberofEquivalentDominoPairs.java
@@ -1,11 +1,16 @@
-// Last updated: 5/4/2025, 12:22:42 PM
+// Last updated: 5/4/2025, 12:23:01 PM
 class Solution {
     public int numEquivDominoPairs(int[][] dominoes) {
-        int[]freq = new int[100];
-        int res = 0;
-        for(int[]dominoe: dominoes) 
-            freq[Math.min(dominoe[0] * 10 + dominoe[1], dominoe[1] * 10 + dominoe[0])]++;
-        for(int i: freq) res += i * (i-1) / 2;
-        return res;
+        int[] freq = new int[100];
+        for(int[] d : dominoes){
+            if(d[0] > d[1])
+                freq[d[0]*10 + d[1]]++;
+            else freq[d[1]*10+ d[0]]++;
+        }
+        int count = 0;
+        for(int f : freq){
+            count += (f-1) * (f) /2;
+        }
+        return count;
     }
 }
