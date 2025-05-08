65f9726ba90893bdc1688bd59102c953153d19bf
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-05-08 15:30:01 -0400

    Create file

---
+++ 209-MinimumSizeSubarraySum/209-MinimumSizeSubarraySum.py
@@ -0,0 +1,15 @@
+# Last updated: 5/8/2025, 3:30:01 PM
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        n = len(nums)
+        ans = n * 2
+        l = 0
+        s = 0
+        for r in range(0, n):
+            s += nums[r]
+            while s - nums[l] >= target:
+                s -= nums[l]
+                l += 1
+            if s >= target:
+                ans = min(ans, r - l + 1)
+        return ans if ans <= n else 0
