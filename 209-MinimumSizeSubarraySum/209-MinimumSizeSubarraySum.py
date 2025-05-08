dce957d5fef0d887827ada4fbd3e0525bf4b6941
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-05-08 15:30:12 -0400

    File updated at 5/8/2025, 3:30:11 PM

--- 209-MinimumSizeSubarraySum/209-MinimumSizeSubarraySum.py
+++ 209-MinimumSizeSubarraySum/209-MinimumSizeSubarraySum.py
@@ -1,8 +1,8 @@
-# Last updated: 5/8/2025, 3:30:01 PM
+# Last updated: 5/8/2025, 3:30:11 PM
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         n = len(nums)
-        ans = n * 2
+        ans = inf
         l = 0
         s = 0
         for r in range(0, n):
