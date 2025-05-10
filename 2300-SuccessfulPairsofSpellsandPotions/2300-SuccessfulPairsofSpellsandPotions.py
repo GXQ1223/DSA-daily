69babdb5bf780986c92dcfeb7e9c8b403283c200
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-05-10 11:18:26 -0400

    File updated at 5/10/2025, 11:18:26 AM

--- 2300-SuccessfulPairsofSpellsandPotions/2300-SuccessfulPairsofSpellsandPotions.py
+++ 2300-SuccessfulPairsofSpellsandPotions/2300-SuccessfulPairsofSpellsandPotions.py
@@ -1,4 +1,7 @@
-# Last updated: 5/10/2025, 11:17:52 AM
+# Last updated: 5/10/2025, 11:18:26 AM
+def works(h: int, citations: List[int]) -> bool:
+    # print(" given researcher has published at least ",h," papers that have each been cited at least ",h," times: ", len(citations) - h >= h)
+    return citations[h] >= len(citations) - h
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         left, right = 0, len(citations) - 1 
@@ -6,10 +9,10 @@ class Solution:
             return 0
         while left <= right:
             mid = (left + right) // 2
-            if citations[mid] >= len(citations) - mid:
+            if works(mid, citations):
                 right = mid - 1
             else:
                 left = mid + 1
-        print("left, right:",left,"; " ,right)
+        # print("left, right:",left,"; " ,right)
         return len(citations) - left
         
