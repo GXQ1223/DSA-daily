4c3781ccfeb3897caab1d0d4cad69fcd5740222c
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-05-30 13:44:30 -0400

    File updated at 5/30/2025, 1:44:30 PM

--- 516-LongestPalindromicSubsequence/516-LongestPalindromicSubsequence.py
+++ 516-LongestPalindromicSubsequence/516-LongestPalindromicSubsequence.py
@@ -1,4 +1,4 @@
-# Last updated: 5/30/2025, 1:43:38 PM
+# Last updated: 5/30/2025, 1:44:30 PM
 class Solution:
     def minScoreTriangulation(self, values: List[int]) -> int:
         n = len(values)
@@ -6,8 +6,6 @@ class Solution:
         def dfs(i,j):
             if i > j-2:
                 return 0
-            if i == j-2:
-                return values[i] * values[i+1] * values[i+2]
             val = inf
             for k in range(i+1, j):
                 val = min(val, dfs(i, k) + dfs(k, j) + values[i] * values[j] * values[k])
