bd59765d1dd03204f1137a2c1ea41a6e9673d986
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-05-30 14:37:33 -0400

    File updated at 5/30/2025, 2:37:33 PM

--- 516-LongestPalindromicSubsequence/516-LongestPalindromicSubsequence.py
+++ 516-LongestPalindromicSubsequence/516-LongestPalindromicSubsequence.py
@@ -1,22 +1,20 @@
-# Last updated: 5/30/2025, 1:59:37 PM
+# Last updated: 5/30/2025, 2:37:33 PM
 class Solution:
-    def minScoreTriangulation(self, values: List[int]) -> int:
-        # n = len(values)
-        # @cache
-        # def dfs(i,j):
-        #     if i > j-2:
-        #         return 0
-        #     val = inf
-        #     for k in range(i+1, j):
-        #         val = min(val, dfs(i, k) + dfs(k, j) + values[i] * values[j] * values[k])
-        #     return val
-        # return dfs(0, n-1)
-
-        n = len(values)
-        f = [[0] * n for _ in range(n)]
-        for i in range(n-3,-1,-1):
-            for j in range(i+2, n):
-                f[i][j] = inf
-                for k in range(i+1, j):
-                    f[i][j] = min(f[i][k] + f[k][j] + values[i]*values[j]*values[k], f[i][j])
-        return f[0][n-1]
+    def mergeStones(self, stones: List[int], k: int) -> int:
+        n = len(stones)
+        if (n-1) % (k-1) != 0: return -1
+        pref = [0] * (n+1)
+        pref[0] = stones[0]
+        for i in range(n):
+            pref[i+1] = pref[i] + stones[i]
+        print("pref: " , pref)
+        
+        @cache
+        def dfs(i,j):
+            if j - i + 1 < k: return 0
+            res = min(dfs(i, mid) + dfs(mid+1, j) for mid in range(i, j, k-1))
+            if (j-i) % (k-1) == 0:
+                res += pref[j+1] - pref[i]
+            return res
+        return dfs(0, n-1)
+        
