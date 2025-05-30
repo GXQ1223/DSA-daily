c586161210449f3740c167557d34c637998dc010
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-05-30 13:43:38 -0400

    File updated at 5/30/2025, 1:43:38 PM

--- 516-LongestPalindromicSubsequence/516-LongestPalindromicSubsequence.py
+++ 516-LongestPalindromicSubsequence/516-LongestPalindromicSubsequence.py
@@ -1,25 +1,15 @@
-# Last updated: 5/30/2025, 1:07:08 PM
+# Last updated: 5/30/2025, 1:43:38 PM
 class Solution:
-    def longestPalindromeSubseq(self, s: str) -> int:
-        # @cache
-        # def dfs(i, j):
-        #     if i > j: # i, j are inclusive
-        #         return 0
-        #     if i == j:
-        #         return 1
-        #     if s[i] == s[j]:
-        #         return 2 + dfs(i+1, j-1)
-        #     else:
-        #         return max(dfs(i+1, j), dfs(i, j-1))
-        # return dfs(0, len(s) - 1)
-
-        n = len(s)
-        f = [[0] * n for _ in range(n)]
-        for i in range(n-1, -1, -1):
-            f[i][i] = 1
-            for j in range(i+1, n):
-                if s[i] == s[j]:
-                    f[i][j] = 2 + f[i+1][j-1]
-                else:
-                    f[i][j] = max(f[i+1][j], f[i][j-1])
-        return f[0][n-1]
+    def minScoreTriangulation(self, values: List[int]) -> int:
+        n = len(values)
+        @cache
+        def dfs(i,j):
+            if i > j-2:
+                return 0
+            if i == j-2:
+                return values[i] * values[i+1] * values[i+2]
+            val = inf
+            for k in range(i+1, j):
+                val = min(val, dfs(i, k) + dfs(k, j) + values[i] * values[j] * values[k])
+            return val
+        return dfs(0, n-1)
