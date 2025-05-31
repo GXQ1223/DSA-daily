1cd0affd24ec9baebd33fb4cbbb99252d67c1122
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-05-31 11:39:06 -0400

    File updated at 5/31/2025, 11:39:06 AM

--- 543-DiameterofBinaryTree/543-DiameterofBinaryTree.py
+++ 543-DiameterofBinaryTree/543-DiameterofBinaryTree.py
@@ -1,4 +1,4 @@
-# Last updated: 5/31/2025, 11:34:59 AM
+# Last updated: 5/31/2025, 11:39:06 AM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -11,16 +11,11 @@ class Solution:
 
         @cache
         def dfs(node):
-            if node is None: return -1001
+            if node is None: return 0
             nonlocal res
-            left = dfs(node.left)
-            right = dfs(node.right)
-            res = max(left, res)
-            res = max(right, res)
-            res = max(left + node.val, res)
-            res = max(right + node.val, res)
+            left = max(0,dfs(node.left))
+            right = max(0,dfs(node.right))
             res = max(left + right + node.val, res)
-            res = max(node.val, res)
             ans = max(max(left, right), 0) + node.val
             # print("ans: " , ans)
             return ans
