fa51cb9a6b035006c350e43aefe6ac4ba148c42b
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-05-31 16:49:37 -0400

    File updated at 5/31/2025, 4:49:37 PM

--- 42-TrappingRainWater/42-TrappingRainWater.py
+++ 42-TrappingRainWater/42-TrappingRainWater.py
@@ -1,41 +1,39 @@
-# Last updated: 5/31/2025, 4:29:30 PM
+# Last updated: 5/31/2025, 4:49:37 PM
 class Solution:
-    def trap(self, height: List[int]) -> int:
-        n = len(height)
-        stack = [0]
-        res = 0
-        for i in range(1,n):
-            # print(stack)
-            if not stack:
-                stack.append(i)
-                continue
-            while stack and height[i] >= height[stack[-1]]:
-                last = stack.pop()
-                if not stack: break
-                nxt = stack[-1]
-                more = (min(height[i], height[nxt]) - height[last]) * (i - nxt - 1)
-                # print("i: ", i, "; last: ",last, "; nxt: ",nxt, "; more: ",more)
-                
-                res += more # height * width
-            stack.append(i)
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        q = deque()
+        res = []
+        for i, num in enumerate(nums):
+            #in
+            while q and num > nums[q[-1]]:
+                q.pop()
+            q.append(i)
+            #out
+            if i - q[0] >= k:
+                q.popleft()
+            #add
+            if i >= k-1:
+                res.append(nums[q[0]])
         return res
 
-
-        #time complexity On
-        #space complexity On
-        # n = len(height)
-        # ans = 0
-        # l = 0
-        # r = n-1
-        # pre_max = 0
-        # suf_max = 0
-        # while l <= r:
-        #     pre_max = max(pre_max, height[l])
-        #     suf_max = max(suf_max, height[r])
-        #     if pre_max < suf_max:
-        #         ans += pre_max - height[l]
-        #         l += 1
-        #     else:
-        #         ans += suf_max - height[r]
-        #         r -= 1
-        # return ans
+        # # Deque to store indices of elements in the window
+        # dq = deque()
+        # res = []
+        
+        # for i in range(len(nums)):
+        #     # Remove elements not within the window
+        #     while dq and dq[0] < i - k + 1:
+        #         dq.popleft()
+            
+        #     # Remove smaller elements from the deque as they are not useful
+        #     while dq and nums[dq[-1]] < nums[i]:
+        #         dq.pop()
+            
+        #     # Add the current element index to the deque
+        #     dq.append(i)
+            
+        #     # Start adding results after the first window is filled
+        #     if i >= k - 1:
+        #         res.append(nums[dq[0]])  # The largest element in the window is at the front of the deque
+        
+        # return res
