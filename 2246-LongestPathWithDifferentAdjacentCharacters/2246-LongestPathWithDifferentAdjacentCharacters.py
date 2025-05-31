daeff6ce4d0f8ccbb4b628371825f36986e6c217
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-05-31 13:54:31 -0400

    File updated at 5/31/2025, 1:54:31 PM

--- 2246-LongestPathWithDifferentAdjacentCharacters/2246-LongestPathWithDifferentAdjacentCharacters.py
+++ 2246-LongestPathWithDifferentAdjacentCharacters/2246-LongestPathWithDifferentAdjacentCharacters.py
@@ -1,4 +1,4 @@
-# Last updated: 5/31/2025, 1:52:09 PM
+# Last updated: 5/31/2025, 1:54:31 PM
 class Solution:
     def minimumDiameterAfterMerge(self, edges1: List[List[int]], edges2: List[List[int]]) -> int:
 
@@ -10,15 +10,12 @@ class Solution:
             return g
         g1, g2 = graph(edges1), graph(edges2)
 
-
         # need to find the starting points first
         def fd(g, start):
             max_depth = 0
             visited = set([start])
             def dfs(x, depth):
-                nonlocal visited
-                nonlocal max_depth
-                nonlocal start
+                nonlocal start, visited, max_depth
                 if depth > max_depth:
                     max_depth = depth
                     start = x
@@ -30,12 +27,7 @@ class Solution:
             dfs(start, 0)
             return start, max_depth
                     
-        start1, _ = fd(g1, 0)
-        start2, _ = fd(g2, 0)
-
-        end1, d1 = fd(g1, start1)
-        end2, d2 = fd(g2, start2)
-
+        (start1, _), (start2, _) = fd(g1, 0), fd(g2, 0)
+        (_, d1), (_, d2) = fd(g1, start1), fd(g2, start2)
 
         return max((d1 + 1)//2 + (d2 + 1)//2 + 1, max(d1, d2))
-        
