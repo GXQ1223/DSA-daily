4d96e5b0fc5396049f4f026098ae51b1c9b8605d
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-05-31 14:18:28 -0400

    File updated at 5/31/2025, 2:18:28 PM

--- 543-DiameterofBinaryTree/543-DiameterofBinaryTree.py
+++ 543-DiameterofBinaryTree/543-DiameterofBinaryTree.py
@@ -1,4 +1,4 @@
-# Last updated: 5/31/2025, 11:39:06 AM
+# Last updated: 5/31/2025, 2:18:28 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,18 +6,17 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        res = -1001
-
+    def rob(self, root: Optional[TreeNode]) -> int:
+        res = 1
         @cache
-        def dfs(node):
-            if node is None: return 0
-            nonlocal res
-            left = max(0,dfs(node.left))
-            right = max(0,dfs(node.right))
-            res = max(left + right + node.val, res)
-            ans = max(max(left, right), 0) + node.val
-            # print("ans: " , ans)
-            return ans
-        dfs(root)
-        return res
+        def dfs(x):
+            if x is None:
+                return 0,0
+            sel_left, nosel_left = dfs(x.left)[0], dfs(x.left)[1]
+            sel_right, nosel_right = dfs(x.right)[0], dfs(x.right)[1]
+
+            nosel_res = max(sel_left, nosel_left) + max(sel_right, nosel_right)
+            sel_res = nosel_left + nosel_right + x.val
+            return sel_res, nosel_res
+        res = dfs(root)
+        return max(res[0], res[1])
