736dceb1da8716d4853ae5979cfb48d1ffe23f0b
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-05-31 15:04:49 -0400

    File updated at 5/31/2025, 3:04:48 PM

--- 2246-LongestPathWithDifferentAdjacentCharacters/2246-LongestPathWithDifferentAdjacentCharacters.py
+++ 2246-LongestPathWithDifferentAdjacentCharacters/2246-LongestPathWithDifferentAdjacentCharacters.py
@@ -1,4 +1,4 @@
-# Last updated: 5/31/2025, 3:04:46 PM
+# Last updated: 5/31/2025, 3:04:48 PM
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
