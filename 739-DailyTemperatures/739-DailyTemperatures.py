f98944420f19df4de54ff50d3a81092ddafd2dd4
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-05-31 15:49:56 -0400

    File updated at 5/31/2025, 3:49:56 PM

--- 739-DailyTemperatures/739-DailyTemperatures.py
+++ 739-DailyTemperatures/739-DailyTemperatures.py
@@ -1,18 +1,31 @@
-# Last updated: 5/31/2025, 3:42:22 PM
+# Last updated: 5/31/2025, 3:49:56 PM
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         n = len(temperatures)
-        stack = []
-        res = []
-        for i in range(n-1, -1, -1):
-            t = temperatures[i]
-            if not stack:
+       
+        # stack = []
+        # res = []
+        # for i in range(n-1, -1, -1):
+        #     t = temperatures[i]
+        #     if not stack:
+        #         stack.append(i)
+        #         res.append(0)
+        #         continue
+        #     while stack and t >= temperatures[stack[-1]] :
+        #         stack.pop()
+        #     res.append(0 if not stack else stack[-1] - i)
+        #     stack.append(i)
+        # res.reverse()
+        # return res
+
+        stack = [0]
+        res = [0] * n
+        for i in range(1, n):
+            if not stack: 
                 stack.append(i)
-                res.append(0)
                 continue
-            while stack and t >= temperatures[stack[-1]] :
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                res[stack[-1]] = i - stack[-1]
                 stack.pop()
-            res.append(0 if not stack else stack[-1] - i)
             stack.append(i)
-        res.reverse()
         return res
