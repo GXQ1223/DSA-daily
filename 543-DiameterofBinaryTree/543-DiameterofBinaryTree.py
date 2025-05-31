e75cb33ca0a0c4e27624625effb416ad02723b53
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-05-31 11:34:26 -0400

    File updated at 5/31/2025, 11:34:26 AM

--- 543-DiameterofBinaryTree/543-DiameterofBinaryTree.py
+++ 543-DiameterofBinaryTree/543-DiameterofBinaryTree.py
@@ -1,4 +1,4 @@
-# Last updated: 5/31/2025, 11:17:11 AM
+# Last updated: 5/31/2025, 11:34:26 AM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,16 +6,22 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        res = 0
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        res = -1001
+
         def dfs(node):
-            if node == None: return -1
-            left = dfs(node.left) + 1
-            right = dfs(node.right) + 1
+            if node is None: return -1001
             nonlocal res
-            res  = max(res, left+right)
-            return max(left, right) 
+            left = dfs(node.left)
+            right = dfs(node.right)
+            res = max(left, res)
+            res = max(right, res)
+            res = max(left + node.val, res)
+            res = max(right + node.val, res)
+            res = max(left + right + node.val, res)
+            res = max(node.val, res)
+            ans = max(max(left, right), 0) + node.val
+            # print("ans: " , ans)
+            return ans
         dfs(root)
         return res
-        
-        return dfs(root, res)[1]
