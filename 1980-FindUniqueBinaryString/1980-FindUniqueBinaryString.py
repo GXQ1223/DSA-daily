2210040860acb8398a439ee4c2f4c061d87830be
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-06-06 15:48:42 -0400

    File updated at 6/6/2025, 3:48:42 PM

--- 1980-FindUniqueBinaryString/1980-FindUniqueBinaryString.py
+++ 1980-FindUniqueBinaryString/1980-FindUniqueBinaryString.py
@@ -1,8 +1,29 @@
-# Last updated: 6/6/2025, 3:33:30 PM
+# Last updated: 6/6/2025, 3:48:42 PM
 class Solution:
-    def findDifferentBinaryString(self, nums: List[str]) -> str:
-        res, ind = [], 0
-        for num in nums:
-            res.append('0' if num[ind] == '1' else '1')
-            ind += 1
-        return "".join(res)
+    def minimumOperations(self, nums: List[int]) -> int:
+        counter = Counter(nums)
+        n = len(nums)
+        freq = 0
+        for c in counter:
+            if counter[c] > 1:
+                freq += 1
+        # print(counter)
+        # print(freq)
+        if freq == 0:
+            return 0
+
+        res = 0
+        cur = 0
+        while cur < n:
+            res += 1
+            for i in range(0,3):
+                counter[nums[cur + i]] -= 1
+                if counter[nums[cur + i]] == 1:
+                    freq -= 1
+                    if freq == 0:
+                        # print("counter: " ,counter, ", res: ",res)
+                        return res
+            cur += 3
+            # print(counter)
+
+        return res + 1
