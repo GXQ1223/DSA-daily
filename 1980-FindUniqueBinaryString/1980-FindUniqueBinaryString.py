b229687177f5d4de9d0362efd5f1dfd787ba5183
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-06-06 15:33:30 -0400

    File updated at 6/6/2025, 3:33:30 PM

--- 1980-FindUniqueBinaryString/1980-FindUniqueBinaryString.py
+++ 1980-FindUniqueBinaryString/1980-FindUniqueBinaryString.py
@@ -1,8 +1,7 @@
-# Last updated: 6/6/2025, 3:33:18 PM
+# Last updated: 6/6/2025, 3:33:30 PM
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
-        res = []
-        ind = 0
+        res, ind = [], 0
         for num in nums:
             res.append('0' if num[ind] == '1' else '1')
             ind += 1
