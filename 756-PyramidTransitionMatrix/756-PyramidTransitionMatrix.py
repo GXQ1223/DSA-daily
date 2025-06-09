642f6486d473aea2ed093a9b0b422d248add5724
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-06-09 15:17:26 -0400

    Create 756-PyramidTransitionMatrix

---
+++ 756-PyramidTransitionMatrix/756-PyramidTransitionMatrix.py
@@ -0,0 +1,30 @@
+# Last updated: 6/9/2025, 3:17:26 PM
+class Solution:
+    def pyramidTransition(self, bottom: str, allowed: List[str]) -> bool:
+        allow = defaultdict(set)
+        ok = False
+        for u,v,w in allowed:
+            allow[u,v].add(w)
+        def dfs(s):
+            nonlocal ok
+            if len(s) == 1: 
+                ok = True
+                return
+            for s1 in build(s, [], 0):
+                dfs(s1)
+                if ok:
+                    return True
+        def build(s, lst, i=0):
+            if i == len(s) - 1:
+                yield "".join(lst)
+            else:
+                u,v = s[i], s[i+1]
+                for w in allow[u, v]:
+                    lst.append(w)
+                    for result in build(s, lst, i+1):
+                        yield result
+                    lst.pop()
+        dfs(bottom)
+        return ok
+
+        
