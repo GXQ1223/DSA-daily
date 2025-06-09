0b17c7d8aac6344a9e6b58df5b635ef78b735639
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-06-09 13:44:38 -0400

    Create 987-VerticalOrderTraversalofaBinaryTree

---
+++ 987-VerticalOrderTraversalofaBinaryTree/987-VerticalOrderTraversalofaBinaryTree.py
@@ -0,0 +1,34 @@
+# Last updated: 6/9/2025, 1:44:37 PM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = defaultdict(list)
+        def dfs(i, ord,lvl):
+            nonlocal res
+            if not i:
+                return
+            if not res[ord]:
+                res[ord] = []
+            res[ord].append((lvl, i.val))
+            dfs(i.left, ord - 1, lvl + 1)
+            dfs(i.right, ord + 1, lvl + 1)
+        dfs(root, 0, 0)
+        ord = 0
+        res2 = []
+        while True:
+            if not res[ord]:
+                break
+            res2.append(sorted(res[ord]))
+            ord += 1
+        ord = -1
+        while True:
+            if not res[ord]:
+                break
+            res2.insert(0, sorted(res[ord]))
+            ord -= 1
+        return [[r[1] for r in group] for group in res2]
