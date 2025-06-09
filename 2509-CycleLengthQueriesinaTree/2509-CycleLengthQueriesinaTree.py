f32559d982c872116821e16571ebc342bc3572f4
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-06-09 15:29:42 -0400

    File updated at 6/9/2025, 3:29:42 PM

--- 2509-CycleLengthQueriesinaTree/2509-CycleLengthQueriesinaTree.py
+++ 2509-CycleLengthQueriesinaTree/2509-CycleLengthQueriesinaTree.py
@@ -1,4 +1,4 @@
-# Last updated: 6/9/2025, 3:29:37 PM
+# Last updated: 6/9/2025, 3:29:42 PM
 from typing import List
 class Solution:
     def cycleLengthQueries(self, n: int, queries: List[List[int]]) -> List[int]:
