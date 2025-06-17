bcbca03e3664a1be0024d1696d97903b9a5119f7
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-06-17 16:26:09 -0400

    File updated at 6/17/2025, 4:26:08 PM

--- 1609-EvenOddTree/1609-EvenOddTree.py
+++ 1609-EvenOddTree/1609-EvenOddTree.py
@@ -1,4 +1,4 @@
-# Last updated: 6/17/2025, 4:25:57 PM
+# Last updated: 6/17/2025, 4:26:08 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -18,13 +18,10 @@ class Solution:
                 seq.append(cur.val)
                 if cur.left: q.append(cur.left)
                 if cur.right: q.append(cur.right)
-            print("seq: ",seq, ";even: ", even)
             original = tuple(seq)
             seq.sort()
             if not even: seq.reverse()
             seq = tuple(seq)
-            print("original: ", original)
-            print("seq: ", seq)
             for i in range(len(seq)):
                 if i > 0 and seq[i]== seq[i-1]: return False
                 if even and seq[i] % 2 == 0: return False
