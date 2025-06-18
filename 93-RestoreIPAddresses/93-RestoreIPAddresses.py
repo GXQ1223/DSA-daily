323fea66a824a8bbf04b6fad26ace2170aa29d03
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-06-18 15:07:05 -0400

    File updated at 6/18/2025, 3:07:05 PM

--- 93-RestoreIPAddresses/93-RestoreIPAddresses.py
+++ 93-RestoreIPAddresses/93-RestoreIPAddresses.py
@@ -1,21 +1,15 @@
-# Last updated: 6/18/2025, 3:04:24 PM
+# Last updated: 6/18/2025, 3:07:05 PM
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
-        # current problem: put s[i] in current number or make it the start of a new number
-        # if s[i] == 0, then if current number is 0, means it can only be the start of new number. 
-            # if current number is not 0, it can be included in current number, or become a single number
-            # if current path has no elements, the 0 must be at the starting position
-        # if s[i] is not 0 it can be the start of new number if current path is shorter than 4.
-            # it can also be appended to the last number of the current path if its within range
+
         ans = set()
         def dfs(i, path):
             nonlocal ans
-            # print("i: ", i, " ;path: ", path)
-            if len(path) > 4 or i > len(s) or (path and int(path[-1]) > 255):return
-            if len(path) < 4 and i == len(s):return
+            if (len(path) > 4 or i > len(s) or (path and int(path[-1]) > 255))\
+                or (len(path) < 4 and i == len(s)):
+                return
             if len(path) == 4 and i == len(s):
-                st = ".".join(path)
-                ans.add(st)
+                ans.add(".".join(path))
                 return
             path.append(s[i])
             dfs(i+1, path)
@@ -27,3 +21,10 @@ class Solution:
                 path[-1] = temp
         dfs(0, [])
         return list(ans)
+
+        # current problem: put s[i] in current number or make it the start of a new number
+        # if s[i] == 0, then if current number is 0, means it can only be the start of new number. 
+            # if current number is not 0, it can be included in current number, or become a single number
+            # if current path has no elements, the 0 must be at the starting position
+        # if s[i] is not 0 it can be the start of new number if current path is shorter than 4.
+            # it can also be appended to the last number of the current path if its within range
