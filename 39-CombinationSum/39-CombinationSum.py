6d125ba836ae26327996c4f93e352fc143240d09
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-06-23 16:28:22 -0400

    File updated at 6/23/2025, 4:28:22 PM

--- 39-CombinationSum/39-CombinationSum.py
+++ 39-CombinationSum/39-CombinationSum.py
@@ -1,4 +1,4 @@
-# Last updated: 6/23/2025, 4:28:13 PM
+# Last updated: 6/23/2025, 4:28:22 PM
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
@@ -7,6 +7,5 @@ class Solution:
         for i in range(1,m): res[i][0] = res[i-1][0] + grid[i][0]
         for i in range(1,n): res[0][i] = res[0][i-1] + grid[0][i]
         for i in range(1, m):
-            for j in range(1, n):
-                res[i][j] = min(res[i-1][j], res[i][j-1]) + grid[i][j]
+            for j in range(1, n): res[i][j] = min(res[i-1][j], res[i][j-1]) + grid[i][j]
         return res[m-1][n-1]
