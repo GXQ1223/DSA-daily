1e42307efacb104194c422c43f33927b054de61d
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-06-25 18:54:47 -0400

    Create 712-MinimumASCIIDeleteSumforTwoStrings

---
+++ 712-MinimumASCIIDeleteSumforTwoStrings/712-MinimumASCIIDeleteSumforTwoStrings.py
@@ -0,0 +1,16 @@
+# Last updated: 6/25/2025, 6:54:47 PM
+class Solution:
+    def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        dp = [[0] * (len(s1)+1) for _ in range(len(s2)+1)]
+        for i in range(1, len(s1)+1): dp[0][i] = ord(s1[i-1]) + dp[0][i-1]
+        for i in range(1, len(s2)+1): dp[i][0] = ord(s2[i-1]) + dp[i-1][0]
+
+        for i in range(1, len(s2)+1):
+            for j in range(1, len(s1)+1):
+                if s2[i-1] == s1[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = min(dp[i-1][j]+ord(s2[i-1]), dp[i][j-1]+ord(s1[j-1]))
+            # print()
+            # for i in dp: print(i)
+        return dp[len(s2)][len(s1)]
