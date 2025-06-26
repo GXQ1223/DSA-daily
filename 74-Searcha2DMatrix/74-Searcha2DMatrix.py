65f75dadea2711c4cc96af00e36445a11c8e971a
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-06-25 20:40:02 -0400

    Create 74-Searcha2DMatrix

---
+++ 74-Searcha2DMatrix/74-Searcha2DMatrix.py
@@ -0,0 +1,31 @@
+# Last updated: 6/25/2025, 8:40:02 PM
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m, n = len(matrix), len(matrix[0])
+
+        # find which row its in matrix[i][0] for i in range(0,m)
+        l,r = 0, m-1
+        while l <= r:
+            mid = (l+r)//2
+            if matrix[mid][0] <= target:
+                l = mid+1
+            else:
+                r = mid-1
+        print(l, ";", r)
+
+        # find which col its in matrix[row][i] for i in range(0,n)
+        row = r
+        l,r = 0, n-1
+        print(l, ";", r,"; row: ",row)
+        while l <= r:
+            mid = (l+r)//2
+            print("matrix[row][mid]: ", matrix[row][mid])
+            if matrix[row][mid] == target or matrix[row][l] == target or matrix[row][r] == target:
+                return True
+            elif matrix[row][mid] < target:
+                l = mid+1
+            else:
+                r = mid-1
+            print(l, "; ",mid,"; ", r)
+        return False
+
