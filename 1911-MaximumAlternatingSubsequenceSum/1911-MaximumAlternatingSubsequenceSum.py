f46e63c512f2b0ae4a40f60a01eb4bf7010003a9
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-07-07 19:06:38 -0400

    File updated at 7/7/2025, 7:06:38 PM

--- 1911-MaximumAlternatingSubsequenceSum/1911-MaximumAlternatingSubsequenceSum.py
+++ 1911-MaximumAlternatingSubsequenceSum/1911-MaximumAlternatingSubsequenceSum.py
@@ -1,10 +1,9 @@
-# Last updated: 7/7/2025, 7:06:21 PM
+# Last updated: 7/7/2025, 7:06:38 PM
 class Solution:
     def maxAlternatingSum(self, nums: List[int]) -> int:
         n = len(nums)
         max_plus = nums[0]
         max_minus = -inf
-        res = 0
         for i in range(1, n):
             temp_plus = max_plus
             summ = max(max_minus + nums[i], max_plus)
@@ -12,8 +11,6 @@ class Solution:
             max_plus = max(max_plus, summ)
             diff = max(max_minus, temp_plus - nums[i])
             max_minus = max(max_minus, diff)
-            res = max(res, summ)
-            res = max(res, diff)
         return max(max_plus, max_minus)
         
                 
