5b0ca020e6461149976f0e9b91cdd59754b1e88a
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-07-22 14:23:04 -0400

    File updated at 7/22/2025, 2:23:04 PM

--- 527-WordAbbreviation/527-WordAbbreviation.py
+++ 527-WordAbbreviation/527-WordAbbreviation.py
@@ -1,4 +1,4 @@
-# Last updated: 7/22/2025, 2:21:49 PM
+# Last updated: 7/22/2025, 2:23:04 PM
 class Solution:
     def wordsAbbreviation(self, words: List[str]) -> List[str]:
         cnt = defaultdict(set)
@@ -15,12 +15,11 @@ class Solution:
                 val = list(val_set)[0]
                 cnt2[val] = key
 
-        # print("cnt2: ", cnt2)
         q = deque()
         for key in cnt.keys():
             if len(cnt[key]) > 1:
                 q.append(key)
-        # print("q: ",q)
+
         # find the shortest uncommon prefix
         while q:
             cur_key = q.pop()
@@ -31,21 +30,16 @@ class Solution:
                 for word in lst:
                     pref = word[:i]
                     key_cnt[pref] += 1
-            keys_set = set(key for key, val in key_cnt.items() if val == 1) 
+            key_lst = sorted(key for key, val in key_cnt.items() if val == 1) 
 
-            # print("keyset: ", keys_set)
-            key_lst = sorted(list(keys_set))
             for key in key_lst:
-                # print("key: ", key)
                 for word in lst:
                     if not cnt2[word] or cnt2[word] == "" :
                         if word.startswith(key):
-                            # print("yes: word: ", word, "; key: ", key)
                             if 0 < len(key) < len(word):
                                 cnt2[word] = key + str(len(word) - len(key) - 1) + word[-1]
                             else:
                                 cnt2[word] = word
-                # print("cnt2: ", cnt2)
 
 
         return [cnt2[key] for key in words]
