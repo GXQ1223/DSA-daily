d6715f7f1016f2ac2428ed14f7f8d3239887e161
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-08-20 01:23:13 -0400

    Create 1277-CountSquareSubmatriceswithAllOnes

---
+++ 1277-CountSquareSubmatriceswithAllOnes/1277-CountSquareSubmatriceswithAllOnes.py
@@ -0,0 +1,18 @@
+# Last updated: 8/20/2025, 1:23:13 AM
+class Solution:
+    def countSquares(self, matrix: List[List[int]]) -> int:
+        res = sum(sum(x) for x in matrix)
+        m,n = len(matrix), len(matrix[0])
+        print(res)
+        change = True
+        for size in range(2, min(m, n) + 1):
+            if not change:
+                break
+            change = False
+            for i in range(m-size+1):
+                for j in range(n-size+1):
+                    if matrix[i][j] != 0 and matrix[i+1][j+1] == matrix[i+1][j] == matrix[i][j+1] == size - 1:
+                        matrix[i][j] = size
+                        change = True
+                        res += 1
+        return res
