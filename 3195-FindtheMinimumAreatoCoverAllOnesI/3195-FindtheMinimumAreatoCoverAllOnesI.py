6105619e88da8788262429e13715e4f3551e469a
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2025-08-22 10:56:01 -0400

    Create 3195-FindtheMinimumAreatoCoverAllOnesI

---
+++ 3195-FindtheMinimumAreatoCoverAllOnesI/3195-FindtheMinimumAreatoCoverAllOnesI.py
@@ -0,0 +1,14 @@
+# Last updated: 8/22/2025, 10:56:01 AM
+class Solution:
+    def minimumArea(self, grid: List[List[int]]) -> int:
+        min_row, min_col = inf, inf
+        max_row, max_col = -inf, -inf
+        m,n = len(grid), len(grid[0])
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    min_row = min(min_row, i)
+                    min_col = min(min_col, j)
+                    max_row = max(max_row, i)
+                    max_col = max(max_col, j)
+        return (max_row - min_row + 1) * (max_col - min_col + 1)
