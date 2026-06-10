9d8cea0283a3aaa612e4316a6d4480bd8fd8dd08
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2026-06-09 19:45:24 -0700

    File updated at 6/9/2026, 10:45:24 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.py
+++ 347-TopKFrequentElements/347-TopKFrequentElements.py
@@ -1,26 +1,23 @@
-# Last updated: 6/9/2026, 10:45:15 PM
+# Last updated: 6/9/2026, 10:45:24 PM
 1class Solution:
 2    def longestConsecutive(self, nums: List[int]) -> int:
 3        s = {x:i for i,x in enumerate(nums)}
 4        counted = {x:False for x in nums}
-5        print(s)
-6        print(counted)
-7        result = 0
-8        for key in s:
-9            if counted.get(key) == True:
-10                continue
-11            #find the smallest ele in the seq
-12            smallest = key
-13            while s.get(smallest-1) is not None:
-14                smallest -= 1
-15            print("smallest: ", smallest)
-16            # find longest seq
-17            largest = smallest
-18            length = 0
-19            while s.get(largest) is not None:
-20                counted[largest] = True
-21                largest += 1
-22                length += 1
-23            result = max(result, length)
-24        return result
-25
+5        result = 0
+6        for key in s:
+7            if counted.get(key) == True:
+8                continue
+9            #find the smallest ele in the seq
+10            smallest = key
+11            while s.get(smallest-1) is not None:
+12                smallest -= 1
+13            # find longest seq
+14            largest = smallest
+15            length = 0
+16            while s.get(largest) is not None:
+17                counted[largest] = True
+18                largest += 1
+19                length += 1
+20            result = max(result, length)
+21        return result
+22
