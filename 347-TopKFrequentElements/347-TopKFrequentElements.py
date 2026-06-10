ed710ff3ee91a8b48f6fafd4ccf01ffc9ebe55e6
Bro_Gua <140037666+GXQ1223@users.noreply.github.com>
2026-06-09 18:26:08 -0700

    File updated at 6/9/2026, 9:26:07 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.py
+++ 347-TopKFrequentElements/347-TopKFrequentElements.py
@@ -1,4 +1,4 @@
-# Last updated: 6/9/2026, 9:26:01 PM
+# Last updated: 6/9/2026, 9:26:07 PM
 1class Solution:
 2    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
 3        counter = {}
@@ -10,9 +10,8 @@
 9        for key in counter.keys():
 10            pairs.append((counter[key], key))
 11        pairs.sort(key = lambda a : (-a[0]))
-12        print(pairs)
-13        res = []
-14        for i in range(k):
-15            res.append(pairs[i][1])
-16        return res
-17        
+12        res = []
+13        for i in range(k):
+14            res.append(pairs[i][1])
+15        return res
+16        
